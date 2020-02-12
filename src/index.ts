58f49caea825adb2e390d118ccb3afcd6b8418d2
Dominic Minischetti <domminischetti@gmail.com>
2020-02-12 15:15:03 -0800

    Update headers for proper CORS support.

--- src/index.ts
+++ src/index.ts
@@ -35,8 +35,8 @@ server.listen(listenPort, () => {
 server.use(express.json());
 
 server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+    res.setHeader("Access-Control-Allow-Headers", "*");
     next();
   });
 
